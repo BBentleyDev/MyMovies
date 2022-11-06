4103e5f4bcfb1fc7172affb4e895bf8df694c4a4
Brandon Bentley <bbentleydev@gmail.com>
2022-11-06 16:54:18 -0500

    created new landing page

--- config/database.js
+++ config/database.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    //Plugging in DB string directly was only way to get heroku host to work
     const conn = await mongoose.connect("mongodb+srv://my:movies@cluster0.lwcydqx.mongodb.net/?retryWrites=true&w=majority", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
